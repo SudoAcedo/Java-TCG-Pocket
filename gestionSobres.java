958111f2d0db4e64f52285fbdc4f3c3b8840c5ca
SudoAcedo <pabloacedolucas@gmail.com>
2026-01-29 12:51:32 +0100

    Update gestionSobres.java

--- gestionSobres.java
+++ gestionSobres.java
@@ -49,31 +49,31 @@ public class gestionSobres {
             estadoJugador.estadoJugador.put("sobres", estadoJugador.estadoJugador.get("sobres") - 1);
 
             HashMap<String, String> pokemon1 = seleccionarCartaAleatoria();
-            System.out.println(utilidades.cargarAsciiArt("C:\\Users\\pablo\\Desktop\\ascii_art\\" + pokemon1.get("Imagen_ASCII")));
+            System.out.println(utilidades.cargarAsciiArt("file" + pokemon1.get("Imagen_ASCII")));
             System.out.println(pokemon1.get("Nombre"));
             System.out.println(pokemon1.get("Rareza"));
             estadoJugador.estadoJugador.put(pokemon1.get("Nombre"), estadoJugador.estadoJugador.get(pokemon1.get("Nombre")) + 1);
 
             HashMap<String, String> pokemon2 = seleccionarCartaAleatoria();
-            System.out.println(utilidades.cargarAsciiArt("C:\\Users\\pablo\\Desktop\\ascii_art\\" + pokemon2.get("Imagen_ASCII")));
+            System.out.println(utilidades.cargarAsciiArt("file" + pokemon2.get("Imagen_ASCII")));
             System.out.println(pokemon2.get("Nombre"));
             System.out.println(pokemon2.get("Rareza"));
             estadoJugador.estadoJugador.put(pokemon2.get("Nombre"), estadoJugador.estadoJugador.get(pokemon2.get("Nombre")) + 1);
 
             HashMap<String, String> pokemon3 = seleccionarCartaAleatoria();
-            System.out.println(utilidades.cargarAsciiArt("C:\\Users\\pablo\\Desktop\\ascii_art\\" + pokemon3.get("Imagen_ASCII")));
+            System.out.println(utilidades.cargarAsciiArt("file" + pokemon3.get("Imagen_ASCII")));
             System.out.println(pokemon3.get("Nombre"));
             System.out.println(pokemon3.get("Rareza"));
             estadoJugador.estadoJugador.put(pokemon3.get("Nombre"), estadoJugador.estadoJugador.get(pokemon3.get("Nombre")) + 1);
 
             HashMap<String, String> pokemon4 = seleccionarCartaAleatoria();
-            System.out.println(utilidades.cargarAsciiArt("C:\\Users\\pablo\\Desktop\\ascii_art\\" + pokemon4.get("Imagen_ASCII")));
+            System.out.println(utilidades.cargarAsciiArt("file" + pokemon4.get("Imagen_ASCII")));
             System.out.println(pokemon4.get("Nombre"));
             System.out.println(pokemon4.get("Rareza"));
             estadoJugador.estadoJugador.put(pokemon4.get("Nombre"), estadoJugador.estadoJugador.get(pokemon4.get("Nombre")) + 1);
 
             HashMap<String, String> pokemon5 = seleccionarCartaAleatoria();
-            System.out.println(utilidades.cargarAsciiArt("C:\\Users\\pablo\\Desktop\\ascii_art\\" + pokemon5.get("Imagen_ASCII")));
+            System.out.println(utilidades.cargarAsciiArt("file" + pokemon5.get("Imagen_ASCII")));
             System.out.println(pokemon5.get("Nombre"));
             System.out.println(pokemon5.get("Rareza"));
             estadoJugador.estadoJugador.put(pokemon5.get("Nombre"), estadoJugador.estadoJugador.get(pokemon5.get("Nombre")) + 1);
@@ -87,7 +87,7 @@ public class gestionSobres {
         Double probabilidadRandom = random.nextDouble(1);
         Double sumaProbabilidades = 0.0;
 
-        ArrayList<HashMap<String, String>> pokemonsData = utilidades.cargarPokemonData("C:\\Users\\pablo\\Desktop\\TCGjava\\pokemon_data.txt");
+        ArrayList<HashMap<String, String>> pokemonsData = utilidades.cargarPokemonData("file");
 
         HashMap<String, String> cartaSeleccionada = new HashMap<>();
 
@@ -106,3 +106,4 @@ public class gestionSobres {
         return cartaSeleccionada;
     }
 }
+
